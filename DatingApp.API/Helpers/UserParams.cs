8cba0e7d39e1396ef113687da99226dc9193f766
Atanas Tankov <atanas.tankov@accedia.com>
2020-03-28 19:14:50 +0200

    Adding Likes functionalities front/back end

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -5,10 +5,10 @@ namespace DatingApp.API.Helpers
         private const int MaxPagesize = 50;
         public int PageNumber { get; set; } = 1;
         private int pageSize = 10;
-        public int PageSize 
+        public int PageSize
         {
-            get {return pageSize;}
-            set {pageSize = (value > MaxPagesize) ? MaxPagesize : value;}
+            get { return pageSize; }
+            set { pageSize = (value > MaxPagesize) ? MaxPagesize : value; }
         }
 
         //Search filtering params
@@ -17,6 +17,8 @@ namespace DatingApp.API.Helpers
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 99;
         public string OrderBy { get; set; }
+        public bool Likees { get; set; } = false;
+        public bool Likers { get; set; } = false;
         // public string KnownAs { get; set; }
     }
 }
